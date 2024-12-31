613b44a6c136447dc73907fb13af70ffe4eebf68
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-12-31 19:34:55 +0100

    added changes

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -18,7 +18,7 @@ const CustomButton = ({
     >
       <span className={`flex-1 ${textStyles}`}>{title}</span>
       {rightIcon && (
-        <div className="relative w-6 h-6">
+        <div className="relative w-6 h-6 border">
           <Image
             src={rightIcon}
             alt="right icon"
