a188d1ad05699d000f2682727d50a6ad88daaa9b
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-12-31 19:29:22 +0100

    fixed changes

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -64,7 +64,7 @@ const CarCard = ({ car }: carCardProps) => {
             containerstyles="w-full py-[16px] rounded-full bg-primary-blue"
             textStyles="text-white text-[14px] leading-[17px] font-bold"
             rightIcon="/right-arrow.svg"
-            handleClick={() => setIsOpen(true)}
+            // handleClick={() => setIsOpen(true)}
           />
         </div>
       </div>
